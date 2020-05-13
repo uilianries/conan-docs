7d768f8fbed0dc45bc2b6fed2c2a070b56ee7ed3
Uilian Ries <uilianries@gmail.com>
2020-05-13 09:27:45 -0300

    Update 404 layout
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conf.py
+++ conf.py
@@ -70,6 +70,7 @@ html_context = {
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+notfound_template = 'layout.html'
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
